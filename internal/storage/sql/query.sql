54aec2b92643f3b603fe18576b3aea36bb2c4395
Alan Night <a.nightails@gmail.com>
2026-06-04 11:25:23 -0500

    Add sqlc and generate the schema and queries for SQL

---
+++ internal/storage/sql/query.sql
@@ -0,0 +1,12 @@
+-- name: CreatePost :one
+INSERT INTO posts (title, description, content, is_draft)
+VALUES (?, ?, ?, ?)
+RETURNING *;
+
+-- name: ListPosts :many
+SELECT * FROM posts
+ORDER BY created_at DESC;
+
+-- name: GetPost :one
+SELECT * FROM posts
+WHERE id = ?;
