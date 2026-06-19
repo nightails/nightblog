cfccbdefa6ef556abe0cebd977010c2dee7c523b
Alan Night <a.nightails@gmail.com>
2026-06-19 13:37:46 -0500

    Refactor config logic to use GoConfig package

--- go.mod
+++ go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/ncruces/go-strftime v1.0.0 // indirect
+	github.com/nightails/goconfig v0.0.0-20260618010006-14511cd4f6b8 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/cobra v1.10.2 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,10 @@
 package config
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/nightails/goconfig"
 )
 
 func configPath() string {
@@ -24,92 +23,25 @@ type Config struct {
 	Editor string
 }
 
-func defaultConfig() Config {
+func defaultConfig() *Config {
 	cacheDir, _ := os.UserCacheDir()
 
-	return Config{
+	return &Config{
 		RemoteBlogsURL: "",
 		DatabasePath:   filepath.Join(cacheDir, "nightblog", "blogs.db"),
 		Editor:         "nvim",
 	}
 }
 
-// Load retrieves an existing configuration or generates a new default configuration if none exists.
-func Load() (*Config, error) {
-	file, err := openConfigFile()
-	if err != nil {
-		// TODO: replace with logger. May need to consolidate, check what being logged in the function first.
-		return nil, fmt.Errorf("failed to open config file: %w", err)
-	}
-	defer file.Close()
-
-	var cfg Config
-	if err := json.NewDecoder(file).Decode(&cfg); err != nil {
-		return nil, err
-	}
-
-	return &cfg, nil
+func (cfg *Config) Load() error {
+	return goconfig.Load(configFilePath(), cfg)
 }
 
-// Write saves the configuration to a JSON file in the user's home directory.
-func Write(cfg *Config) error {
-	data, err := json.MarshalIndent(cfg, "", "  ")
-	if err != nil {
-		return err
-	}
-	if err := os.WriteFile(configFilePath(), data, 0o755); err != nil {
-		return err
-	}
-	return nil
+func (cfg *Config) Save() error {
+	return goconfig.Save(configFilePath(), cfg)
 }
 
-// openConfigFile retrieves the configuration file for the application.
-// NOTE: Close the file after use to prevent resource leaks.
-func openConfigFile() (*os.File, error) {
-	// create the config directory if not exist
-	_, err := os.Stat(configPath())
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			if err := os.MkdirAll(configPath(), 0o755); err != nil {
-				// TODO: replace with logger
-				return nil, fmt.Errorf("failed to create config directory: %w", err)
-			}
-		} else {
-			return nil, fmt.Errorf("failed to stat config path: %w", err)
-		}
-	}
-
-	// open or create the config file if not exist
-	file, err := os.Open(configFilePath())
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			file, err = os.Create(configFilePath())
-			if err != nil {
-				// TODO: replace with logger
-				return nil, fmt.Errorf("failed to create config file: %w", err)
-			}
-			data, err := json.MarshalIndent(defaultConfig(), "", "  ")
-			if err != nil {
-				file.Close()
-				// TODO: replace with logger
-				return nil, fmt.Errorf("failed to marshal default config: %w", err)
-			}
-			// write default config to file
-			_, err = file.Write(data)
-			if err != nil {
-				file.Close()
-				// TODO: replace with logger
-				return nil, fmt.Errorf("failed to write default config to file: %w", err)
-			}
-			// rest cursor to the beginging of the file
-			if _, err := file.Seek(0, 0); err != nil {
-				file.Close()
-				return nil, fmt.Errorf("failed to seek the begining of the config file: %w", err)
-			}
-		} else {
-			return nil, fmt.Errorf("failed to open config file: %w", err)
-		}
-	}
-
-	return file, nil
+func (cfg *Config) CreateDefault() error {
+	cfg = defaultConfig()
+	return goconfig.Create(configFilePath(), cfg)
 }
